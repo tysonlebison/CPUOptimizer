5648ab0ba2752abeeb774cd57a4800230ad013d7
tysonlebison <tysonlebison@gmail.com>
2017-04-21 08:19:38 -0600

    test

--- CPUOptimizer/Form1.Designer.cs
+++ CPUOptimizer/Form1.Designer.cs
@@ -33,21 +33,19 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(41, 53);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(62, 82);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(141, 29);
+            this.button1.Size = new System.Drawing.Size(212, 45);
             this.button1.TabIndex = 0;
             this.button1.Text = "AYYY LMAO";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(356, 270);
+            this.ClientSize = new System.Drawing.Size(534, 415);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
